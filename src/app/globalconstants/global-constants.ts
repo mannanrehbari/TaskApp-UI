838ce9f723427bdb0852f1c5c5f62a703a87b59c
Abdul Mannan <abdul.mannan@mavs.uta.edu>
2021-01-16 14:55:23 -0600

    SSL on API server
    SSL on API server

--- src/app/globalconstants/global-constants.ts
+++ src/app/globalconstants/global-constants.ts
@@ -1,9 +1,9 @@
 export class GlobalConstants {
 
-    // public static SERVER_IP: string = "http://139.59.12.233";
-    public static SERVER_IP: string = "http://localhost";
+    public static SERVER_IP: string = "https://rest-apis.iwork.pk";
+    // public static SERVER_IP: string = "http://localhost";
 
-    public static SERVER_PORT: string = "9091";
+    public static SERVER_PORT: string = "8443";
     public static SERVER_ADDRESS: string = GlobalConstants.SERVER_IP + ':' + GlobalConstants.SERVER_PORT + '/';
 
     //api version prefix
